38cf028591af987000a6c152899dcbc66409b0ba
sole <sole@mozilla.com>
2016-03-02 15:41:28 +0000

    basicest of blocks

---
+++ SiteSetup.js
@@ -0,0 +1,41 @@
+var fs = require('fs');
+var path = require('path');
+var Handlebars = require('handlebars');
+
+module.exports = function SiteSetup(args) {
+	this.doIt = function() {
+		console.log('doing it');
+		console.log(args);
+
+		completeArguments(args);
+
+		console.log('completed');
+		console.log(args);
+		console.log('---------');
+
+		// nginx config file
+		var siteConfig = generateNginxConfigFile(args);
+		console.log(siteConfig);
+
+
+		// nginx ln -s sites-enabled
+		// create site folder(s)
+		// database
+	}
+
+	function completeArguments(args) {
+		if(!args.directory) {
+			args.directory = args.domain;
+		}
+
+		args.server_name = args.domain;
+		args.site_path = path.join(args.www_path, args.directory);
+	}
+
+	function generateNginxConfigFile(args) {
+		var source = fs.readFileSync('./data/server.template.conf', 'utf-8');
+		var template = Handlebars.compile(source);
+		var result = template(args);
+		return result;
+	}
+}
---
+++ index.js
@@ -0,0 +1,39 @@
+var SiteSetup = require('./SiteSetup');
+var ArgParse = require('argparse').ArgumentParser;
+
+var parser = new ArgParse({
+	addHelp: true,
+	description: 'Set up sites, quick'
+});
+
+// pastelito-site-setup —with-php —with-https-redirect —create-database -domain=mydomain.com -directory=mydomain.com -www-path=/var/www/ -nginx-conf-path=/etc/nginx/
+
+// domain -> directory
+parser.addArgument(
+	['--domain'],
+	{
+		help: 'Domain of the site. E.g. example.com',
+		required: true
+	}
+);
+
+parser.addArgument(
+	['--www-path'],
+	{
+		help: 'Folder where website directories are. Default is /var/www',
+		defaultValue: '/var/www'
+	}
+);
+
+// https certificate (?)
+// with-php
+// with-https-redirect
+// with-www-redirect
+// www-path assumed /var/www
+// nginx-conf-path assumed /etc/nginx
+// with-database [name] assumed domain name
+
+var args = parser.parseArgs();
+var setup = new SiteSetup(args);
+
+setup.doIt();
